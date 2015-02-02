78fb4a7e895e0291c715d19d0f5a2a26aeb87c0e
Thomas Kemmer <thomas@binaryden.de>
2015-02-02 18:30:03 +0100

    Minor refactoring

--- test/hmo.jl
+++ test/hmo.jl
@@ -6,25 +6,20 @@ const testtypes = (Float64, Float32)
 
 const tempfile = mktemp()
 const testfiles = ((tempfile[1], tempfile[2], (0,0,0)),
-				   ("./hmo.hmo", open("./test/hmo.hmo"), (2,2,2)))
+				   ("./test/hmo.hmo", open("./test/hmo.hmo"), (2,2,2)))
 
 try
-	for (fname, fh, len) in testfiles
-		try
-			for dtype in testtypes
-				nodes, elements, charges = readhmo(fh, dtype=dtype)
-				@test isa(nodes, Vector{Vector{dtype}})
-				@test isa(elements, Vector{Element{dtype}})
-				@test isa(charges, Vector{Charge{dtype}})
-				@test length(nodes) == len[1]
-				@test length(elements) == len[2]
-				@test length(charges) == len[3]
-				seek(fh,0)
-			end
-		finally
-			close(fh)
-		end
+	for (fname, fh, len) in testfiles, dtype in testtypes
+		nodes, elements, charges = readhmo(fh, dtype=dtype)
+		@test isa(nodes, Vector{Vector{dtype}})
+		@test isa(elements, Vector{Element{dtype}})
+		@test isa(charges, Vector{Charge{dtype}})
+		@test length(nodes) == len[1]
+		@test length(elements) == len[2]
+		@test length(charges) == len[3]
+		seek(fh,0)
 	end
 finally
-	rm(testfiles[1][1])
+	[close(fh) for (_, fh, __) in testfiles]
+	rm(tempfile[1])
 end
