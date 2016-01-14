943fb14f0ca40ec6760f89ab75b7a6c0f5f9091d
Thomas Kemmer <thomas@binaryden.de>
2016-01-14 16:18:49 +0100

    Fix: readmatlab* functions should now actually work

--- src/input/matlab.jl
+++ src/input/matlab.jl
@@ -8,13 +8,8 @@
     @return (Vector{Vector{T}}, Vector{Tetrahedron{T}})
 =#
 function readmatlab{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
-    nodes = Vector{T}[]; elements = Tetrahedron{T}[]
-    while !eof(stream)
-        line = readline(stream)
-        startswith(line, "vert=[") && (nodes = readmatlab_nodes(stream, T))
-        startswith(line, "simp=[") && (elements = readmatlab_elements(stream, nodes, T))
-    end
-    (nodes, elements)
+    nodes = readmatlab_nodes(stream, T)
+    (nodes, readmatlab_elements(stream, nodes, T))
 end
 
 #=
@@ -28,10 +23,9 @@ end
 =#
 function readmatlab_nodes{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
     nodes = Vector{T}[]
+    seek(stream, "vert=[")
     while !eof(stream)
         line = readline(stream)
-        startswith(line, "vert=[") || continue   # fast-forward to nodes section
-        startswith(line, "vert=[") && continue   # skip first line of the section
         startswith(line, "%") && continue        # skip comments
         startswith(line, "];") && break          # all nodes read
         push!(nodes, [parse(T, e) for e in split(line)[3:5]])
@@ -52,10 +46,9 @@ end
 =#
 function readmatlab_elements{T <: AbstractFloat}(stream::IOStream, nodes::Vector{Vector{T}}, ::Type{T}=Float64)
     elements = Tetrahedron{T}[]
+    seek(stream, "simp=[")
     while !eof(stream)
         line = readline(stream)
-        startswith(line, "simp=[") || continue   # fast-forward to simplices section
-        startswith(line, "simp=[") && continue   # skip first line of the section
         startswith(line, "%") && continue        # skip comments
         startswith(line, "];") && break          # all simplices read
         push!(elements, Tetrahedron([nodes[parse(Int, e)+1] for e in split(line)[8:end]]...))
