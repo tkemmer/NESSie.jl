a10bbc4008ae178ccab7f2d7b8dbdac8da68becb
Thomas Kemmer <thomas@binaryden.de>
2016-11-21 17:59:24 +0100

    Add: Local φΩ potential computation

---
+++ src/bem/local/potentials.jl
@@ -0,0 +1,16 @@
+#=
+    TODO
+=#
+function φΩ{T}(Ξ::Vector{Vector{T}}, bem::LocalBEMResult{T}, LaplaceMod::Module=Rjasanow)
+    # result vector
+    φ = zeros(T, length(Ξ))
+
+    # φ  = -[W ⋅ u](ξ)
+    LaplaceMod.laplacecoll!(DoubleLayer, φ, bem.model.elements, Ξ, bem.u)
+    scale!(φ, -1)
+
+    # φ += [Vtilde ⋅ q](ξ)
+    LaplaceMod.laplacecoll!(SingleLayer, φ, bem.model.elements, Ξ, bem.q)
+
+    T(1,69e-9 / 4π / ε0) * (4π \ φ + opt.εΩ \ φmol(Ξ, charges)) # [φΩ] = V = C/F
+end
