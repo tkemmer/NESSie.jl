658f96b5ea5ba794c9e30ae4f5edb6eee519d849
Thomas Kemmer <thomas@binaryden.de>
2016-11-23 11:39:28 +0100

    Fix: Typo

--- src/base/constants.jl
+++ src/base/constants.jl
@@ -6,7 +6,7 @@
         Finite Elemente und Randelemente (in German). Advances in Numerical Matheamtics.
         Teubner Verlag/GWV Fachverlage GmbH, Wiesbaden, 2003.
 =#
-const ε0 = 1 / (4π * 1e-7 * 299792458^2)    # vacuum permittivity [F/m]
+const ε0 = 1 / (4π * 1e-7 * 299792458^2)    # vacuum permittivity [ε0] = F/m
 const σ  = 0.5                              # σ(ξ) = lim_{ε→0} 1/4πε² ∫dΓᵣ = 1/2 for almost all ξ ∈ Γ [1]
 
 #=
