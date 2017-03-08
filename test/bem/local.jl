b189f948384a0bdec4a8c1e696e44700ff44e462
Thomas Kemmer <thomas@binaryden.de>
2017-03-08 17:32:08 +0100

    Add: Test dummies for rfenergy (BEM)

--- test/bem/local.jl
+++ test/bem/local.jl
@@ -1,3 +1,4 @@
 @pending solvelocal --> :nothing
 @pending φΩ --> :nothing
 @pending φΣ --> :nothing
+@pending rfenergy --> :nothing
--- test/bem/nonlocal.jl
+++ test/bem/nonlocal.jl
@@ -1,3 +1,4 @@
 @pending solvenonlocal --> :nothing
 @pending φΩ --> :nothing
 @pending φΣ --> :nothing
+@pending rfenergy --> :nothing
