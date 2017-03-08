0aaeaf931851ac989fa53f4b196a5279350dd52c
Thomas Kemmer <thomas@binaryden.de>
2017-03-08 17:14:14 +0100

    Add: Local reaction field energy (BEM)

--- src/BEM.jl
+++ src/BEM.jl
@@ -1,16 +1,20 @@
 module BEM
 
 using ..ProteinES
-using ..ProteinES: Radon, Rjasanow, σ, potprefactor, pluseye!, gemv!, gemv, gemm, axpy!, yukawa
+using ..ProteinES: Radon, Rjasanow, σ, ec, potprefactor, pluseye!, gemv!, gemv, gemm, axpy!, yukawa
 using Distances: euclidean
 
 import ..ProteinES: φΩ, φΣ
-export solve, φΩ, φΣ
+export solve, φΩ, φΣ, rfenergy
 
 abstract type BEMResult{T <: AbstractFloat} end
 
+# local electrostatics
 include("bem/local/solver.jl")
 include("bem/local/potentials.jl")
+include("bem/local/energy.jl")
+
+# nonlocal electrostatics
 include("bem/nonlocal/solver.jl")
 include("bem/nonlocal/potentials.jl")
 
---
+++ src/bem/local/energy.jl
@@ -0,0 +1,43 @@
+#=
+    Computes the local reaction field energy W* as
+
+        W* = ∫φ*⋅ρ dΩ
+
+    where φ* is the reaction field and ρ is the corresponding charge distribution.
+
+    @param bem
+        Result of `solvelocal`
+    @param LaplaceMod
+        Module to be used for Laplace potential; Valid values: Radon, Rjasanow
+    @return T ([W*] = kJ/mol)
+=#
+function rfenergy{T}(bem::LocalBEMResult{T}, LaplaceMod::Module=Rjasanow)
+    qposs = [charge.pos for charge in bem.model.charges]
+    qvals = [charge.val for charge in bem.model.charges]
+
+    # reaction field energy per point charge
+    wstar = zeros(T, length(bem.model.charges))
+
+    # W* = -[W ⋅ u](ξ)
+    LaplaceMod.laplacecoll!(DoubleLayer, wstar, bem.model.elements, qposs, bem.u)
+    scale!(wstar, -1)
+
+    # W* += [V ⋅ q](ξ)
+    LaplaceMod.laplacecoll!(SingleLayer, wstar, bem.model.elements, qposs, bem.q)
+
+    # Apply ρ
+    wstar .*= qvals
+
+    # Integrate and apply remaining prefactors (in order)
+    # ▶ 4π        for Vtilde, W
+    # (in potprefactor:)
+    # ▶ 4π⋅ε0     for u and q
+    # ▶ 1.602e-19 for elemental charge e; [e] = C
+    # ▶ 1e10      for the conversion Å → m; [ε0] = F/m
+    #
+    # ▶ 1.602e-19 for elemental charge e; [e] = C
+    # ▶ 6.022e23  for Avogadro constant Nₐ; [Nₐ] = 1/mol
+    # ▶ 1e-3       for the conversion 1/J → 1/kJ
+    # ▶ 0.5       since we have counted all interactions twice
+    sum(wstar) / 4π * potprefactor(T) * ec * 6.022140857e10 / 2
+end
