7583a92a35c79fb6fdd6740c255b77af8d9841aa
Thomas Kemmer <thomas@binaryden.de>
2017-04-10 19:22:47 +0200

    [IO] Clean up module file

--- src/IO.jl
+++ src/IO.jl
@@ -5,14 +5,25 @@ using ..ProteinES: reverseindex, unpack, vertexnormals
 using JSON: json
 using LightXML: XMLDocument, create_root, new_child, add_text, set_attribute
 
-export readhmo, readmcsf, readmsms, readoff, readpqr, writeskel, writexml3d_json, writexml3d_xml
-
 include("io/hmo.jl")
+export readhmo
+
 include("io/mcsf.jl")
+export readmcsf
+
 include("io/msms.jl")
+export readmsms
+
 include("io/off.jl")
+export readoff
+
 include("io/pqr.jl")
+export readpqr
+
 include("io/skel.jl")
+export writeskel
+
 include("io/xml3d.jl")
+export writexml3d_json, writexml3d_xml
 
 end # module
