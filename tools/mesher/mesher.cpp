e367327f038f50b0b9932eedbdf8b57ae68dd3d3
Thomas Kemmer <thomas@binaryden.de>
2017-04-12 10:22:34 +0200

    [Tool/Mesher] Beautify output

--- tools/mesher/mesher.cpp
+++ tools/mesher/mesher.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 void printUsage() {
-    cout << "\n\033[1mProteinES.jl molecule mesher (using GAMer)\033[0m\n"
+    cout << "\n\e[1mProteinES.jl molecule mesher (using GAMer)\e[0m\n"
          << "==========================================\n\n"
-         << "\033[1mUsage:\033[0m\n\n"
+         << "\e[1mUsage:\e[0m\n\n"
          << "\tmesher <file in> <dir out> [<option>...]\n"
-         << "\n\033[1mSupported input formats:\033[0m\n\n"
-         << "\t\033[1mPDB\033[0m\t*.pdb\n"
-         << "\t\033[1mPQR\033[0m\t*.pqr\n"
-         << "\n\033[1mSupported options (with default values):\033[0m\n\n";
+         << "\n\e[1mSupported input formats:\e[0m\n\n"
+         << "\t\e[1mPDB\e[0m\t*.pdb\n"
+         << "\t\e[1mPQR\e[0m\t*.pqr\n"
+         << "\n\e[1mSupported options (with default values):\e[0m\n\n";
     for(const auto& e: options)
-        cout << "\t\033[1m--" << e.first << "=<" << e.second.type << ">\033[0m\t"
+        cout << "\t\e[1m--" << e.first << "=<" << e.second.type << ">\e[0m\t"
              << e.second.defval << "\t" << e.second.desc << "\n";
     cout << endl;
 }
@@ -27,30 +27,30 @@ int main(int argc, char** argv) {
     string out_dir {argv[2]};
     auto opt = parseOptions(&argv[3], &argv[argc]);
 
-    cout << "\033[1mGenerating surface mesh...\033[0m\n";
+    cout << "\e[1mGenerating surface mesh...\e[0m\n";
     Surface molsurf;
     AtomList atoms;
     tie(molsurf, atoms) = gaussianSurfaceFromPDB(pdb_in, opt.iso_value);
 
-    cout << "\033[1mSmoothen surface mesh...\033[0m\n";
+    cout << "\e[1mSmoothen surface mesh...\e[0m\n";
     smoothSurfaceMesh(molsurf, opt);
 
-    cout << "\033[1mWriting surface mesh...\033[0m\n";
+    cout << "\e[1mWriting surface mesh...\e[0m\n";
     SurfaceMesh_writeOFF(molsurf.data(), &(out_dir + "/mesh.off")[0]);
 
-    cout << "\033[1mGenerating bounding sphere...\033[0m\n";
+    cout << "\e[1mGenerating bounding sphere...\e[0m\n";
     auto center = SurfaceMesh_getCenterRadius(molsurf.data());
     auto sphere = generateBoundingSphere(center, opt);
     cout << "Molecule radius: " << center.radius << ", Center: ["
          << center.x << ", " << center.y << ", " << center.z << "]\n";
 
-    cout << "\033[1mGenerating volume mesh...\033[0m\n";
+    cout << "\e[1mGenerating volume mesh...\e[0m\n";
     Surface mesh = SurfaceMesh_merge(molsurf.data(), sphere.data());
-    auto domain = generateVolumeMesh(mesh, atoms);
+    auto domain  = generateVolumeMesh(mesh, atoms);
 
-    cout << "\033[1mWriting volume mesh...\033[0m\n";
-    cout << "\033[1m * Inner mesh...\033[0m\n";
+    cout << "\e[1mWriting volume mesh...\e[0m\n";
+    cout << "\e[1m * Inner mesh...\e[0m\n";
     writeMcsf(out_dir, *domain, true, opt.sphere_ratio, center);
-    cout << "\033[1m * Outer mesh...\033[0m\n";
+    cout << "\e[1m * Outer mesh...\e[0m\n";
     writeMcsf(out_dir, *domain, false, opt.sphere_ratio, center);
 }
