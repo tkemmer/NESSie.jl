8e00991aa8b8e8b81053c45360293d44122120b6
Thomas Kemmer <thomas@binaryden.de>
2017-04-24 18:24:28 +0200

    [IO] Documentify IO/hmo

--- doc/src/io/input.md
+++ doc/src/io/input.md
@@ -1,1 +1,33 @@
 # Input file support
+
+```@meta
+    CurrentModule = ProteinES.IO
+```
+
+Currently supported input file formats with different models:
+
+| File type          | Surface model | Volume model | Charges included |
+|--------------------|:-------------:|:------------:|:----------------:|
+| [HMO](@ref)        | ✓             |              | ✓                |
+
+## HMO
+```@docs
+    readhmo
+```
+
+## Internal
+```@meta
+    DocTestSetup = quote
+        using ProteinES.IO: readhmo_nodes, readhmo_elements, readhmo_charges
+    end
+```
+
+```@docs
+    readhmo_nodes
+    readhmo_elements
+    readhmo_charges
+```
+
+```@meta
+    DocTestSetup = nothing
+```
--- src/io/hmo.jl
+++ src/io/hmo.jl
@@ -1,27 +1,40 @@
-#=
-    Reads all data from the given HMO file.
+# =========================================================================================
+"""
+    readhmo{T <: AbstractFloat}(
+        stream::IOStream,
+              ::Type{T}=Float64
+    )
 
-    @param stream
-        Handle to HMO file
-    @param _
-        Data type T for return value
-    @return SurfaceModel{T}
-=#
+Reads a complete surface model from the given HMO file.
+
+# Return type
+`SurfaceModel{T}`
+
+# Alias
+
+    readhmo{T}(fname::String, ::Type{T}=Float64)
+
+Reads the model using a file name rather than a `IOStream` object.
+"""
 function readhmo{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
     nodes = readhmo_nodes(stream, T)
     SurfaceModel(nodes, readhmo_elements(stream, nodes, T), readhmo_charges(stream, T))
 end
 readhmo{T}(fname::String, ::Type{T}=Float64) = open(fh -> readhmo(fh, T), fname)
 
-#=
-    Reads all node data from the given HMO file.
 
-    @param stream
-        Handle to HMO file
-    @param _
-        Data type T for return value
-    @return Vector{Vector{T}}
-=#
+# =========================================================================================
+"""
+    readhmo_nodes{T <: AbstractFloat}(
+        stream::IOStream,
+              ::Type{T}=Float64
+    )
+
+Reads all nodes from the given HMO file.
+
+# Return type
+`Vector{Vector{T}}`
+"""
 function readhmo_nodes{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
     nodes = Vector{T}[]
     seek(stream, "BEG_NODL_DATA")
@@ -33,18 +46,25 @@ function readhmo_nodes{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
     nodes
 end
 
-#=
-    Reads all element data from the given HMO file.
 
-    @param stream
-        Handle to HMO file
-    @param nodes
-        List of referenced nodes
-    @param _
-        Data type T for return value
-    @return Vector{Vector{T}}
-=#
-function readhmo_elements{T <: AbstractFloat}(stream::IOStream, nodes::Vector{Vector{T}}, ::Type{T}=T)
+# =========================================================================================
+"""
+    readhmo_elements{T <: AbstractFloat}(
+        stream::IOStream,
+        nodes ::Vector{Vector{T}},
+              ::Type{T}=Float64
+    )
+
+Reads all elements from the given HMO file.
+
+# Return type
+`Vector{Triangle{T}}`
+"""
+function readhmo_elements{T <: AbstractFloat}(
+        stream::IOStream,
+        nodes::Vector{Vector{T}},
+        ::Type{T}=T
+    )
     elements = Triangle{T}[]
     seek(stream, "BEG_ELEM_DATA")
     readline(stream) # skip first line
@@ -55,15 +75,19 @@ function readhmo_elements{T <: AbstractFloat}(stream::IOStream, nodes::Vector{Ve
     elements
 end
 
-#=
-    Reads all charge data from the given HMO file.
 
-    @param stream
-        Handle to HMO file
-    @param _
-        Data type T for return value
-    @return Vector{Charge{T}}
-=#
+# =========================================================================================
+"""
+    readhmo_charges{T <: AbstractFloat}(
+        stream::IOStream,
+              ::Type{T}=Float64
+    )
+
+Reads all charges from the given HMO file.
+
+# Return type
+`Vector{Charge{T}}`
+"""
 function readhmo_charges{T <: AbstractFloat}(stream::IOStream, ::Type{T}=Float64)
     charges = Charge{T}[]
     seek(stream, "BEG_CHARGE_DATA")
