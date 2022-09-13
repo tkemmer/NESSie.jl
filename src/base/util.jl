7c16895c2ecee36261de789130dfec54b55d818d
Thomas Kemmer <thomas@binaryden.de>
2022-09-13 13:31:34 +0200

    [Base] Fix obspoint_* return type documentation

--- src/base/util.jl
+++ src/base/util.jl
@@ -429,7 +429,7 @@ end
 Generates `n` evenly distributed observation points along the line segment from `u` to `v`.
 
 # Return type
-`Function`
+`Generator -> Vector{T}`
 
 # Example
 ```julia
@@ -462,7 +462,7 @@ to the points ``A``, ``B``, and ``C``, respectively.
  * `nbc`   Number of observation points along ``\\overline{BC}``
 
 # Return type
-`Function`
+`Generator -> Vector{T}`
 
 # Example
 ```julia
