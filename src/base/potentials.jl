221c3039404b61d0e4f232e017297d3b9e7178af
Thomas Kemmer <thomas@binaryden.de>
2025-02-14 16:23:09 +0100

    Base: refactor molecular potentials, unify interface

--- src/base/potentials.jl
+++ src/base/potentials.jl
@@ -35,10 +35,10 @@ struct LocalES    <: LocalityType end
 # =========================================================================================
 """
     φmol(
-        ξ        ::Vector{T},
-        charges  ::Vector{Charge{T}};
-        # kwargs
-        tolerance::T                 = T(1e-10)
+        ξ::Vector{T},
+        charges::AbstractVector{Charge{T}};
+        # keyword arguments
+        tolerance::T = T(1e-10)
     )
 
 Computes and returns the molecular potential of the given system of point charges in a
@@ -59,51 +59,50 @@ for the affected charge.
 
 ## Aliases
     φmol{T}(
-        Ξ        ::Vector{Vector{T}},
-        charges  ::Vector{Charge{T}};
-        # kwargs
-        tolerance::T                 = T(1e-10)
+        Ξ::AbstractVector{Vector{T}},
+        charges::AbstractVector{Charge{T}};
+        # keyword arguments
+        tolerance::T = T(1e-10)
     )
 
 Computes the molecular potentials for a list of observation points.
 
     φmol{T}(
-        model    ::Model{T, Triangle{T}};
-        # kwargs
-        tolerance::T                     = T(1e-10)
+        model::Model{T, Triangle{T}};
+        # keyword arguments
+        tolerance::T = T(1e-10)
     )
 
 Computes the molecular potentials for the given surface model, using each triangle center
 as observation point.
 """
-function φmol(
-        ξ        ::Vector{T},
-        charges  ::Vector{Charge{T}};
-        tolerance::T=T(1e-10)
-    ) where T
-    isempty(charges) ? zero(T) :
-        sum(q.val / max(euclidean(ξ, q.pos), tolerance) for q in charges)
+@inline function φmol(
+    ξ::Vector{T},
+    charges::AbstractVector{Charge{T}};
+    tolerance::T=T(1e-10)
+) where T
+    sum(q.val / max(euclidean(ξ, q.pos), tolerance) for q in charges; init = zero(T))
 end
 
 @inline function φmol(
-        Ξ        ::Vector{Vector{T}},
-        charges  ::Vector{Charge{T}};
-        tolerance::T=T(1e-10)
-    ) where T
-    [φmol(ξ, charges, tolerance=tolerance) for ξ in Ξ]
+    Ξ::Union{AbstractVector{Vector{T}}, <: Base.Generator},
+    charges::AbstractVector{Charge{T}};
+    kwargs...
+) where T
+    φmol.(Ξ, Ref(charges); kwargs...)
 end
 
 @inline function φmol(
-        model    ::Model{T, Triangle{T}};
-        tolerance::T=T(1e-10)
-    ) where T
-    [φmol(ξ.center, model.charges, tolerance=tolerance) for ξ in model.elements]
+    model::Model{T, Triangle{T}};
+    kwargs...
+) where T
+    φmol((elem.center for elem in model.elements), model.charges; kwargs...)
 end
 
 
 # =========================================================================================
 """
-    ∂ₙφmol(ξ::Triangle{T}, charges::Vector{Charge{T}})
+    ∂ₙφmol(ξ::Triangle{T}, charges::AbstractVector{Charge{T}})
 
 Computes and returns the normal derivative of the given system's molecular potential in a
 structureless medium, using the given triangle's center as observation point and the
@@ -120,27 +119,37 @@ triangle's normal as reference normal.
 `T`
 
 ## Aliases
+    ∂ₙφmol(Ξ::AbstractVector{Triangle{T}}, charges::AbstractVector{Charge{T}})
+
+Computes the normal derivatives of the molecular potentials for all given surface triangles.
+
     ∂ₙφmol(model::Model{T, Triangle{T}})
 
 Computes the normal derivatives of the molecular potentials for the given surface model,
 using each triangle center and normal as observation point.
 """
-function ∂ₙφmol(
-        ξ      ::Triangle{T},
-        charges::Vector{Charge{T}}
-    ) where T
-    isempty(charges) ? zero(T) :
-        -sum(q.val * ddot(ξ.center, q.pos, ξ.normal) / euclidean(ξ.center, q.pos)^3 for q in charges)
+@inline function ∂ₙφmol(
+    ξ::Triangle{T},
+    charges::AbstractVector{Charge{T}}
+) where T
+    -sum(q.val * ddot(ξ.center, q.pos, ξ.normal) / euclidean(ξ.center, q.pos)^3 for q in charges; init = zero(T))
+end
+
+@inline function ∂ₙφmol(
+    Ξ::Union{AbstractVector{Triangle{T}}, <: Base.Generator},
+    charges::AbstractVector{Charge{T}}
+) where T
+    ∂ₙφmol.(Ξ, Ref(charges))
 end
 
 @inline function ∂ₙφmol(model::Model{T, Triangle{T}}) where T
-    [∂ₙφmol(ξ, model.charges) for ξ in model.elements]
+    ∂ₙφmol(model.elements, model.charges)
 end
 
 
 # =========================================================================================
 """
-    ∇φmol(ξ::Vector{T}, charges::Vector{Charge{T}})
+    ∇φmol(ξ::Vector{T}, charges::AbstractVector{Charge{T}})
 
 Computes and returns the gradient of the given system's molecular potential in a
 structureless medium for the given observation point ξ.
@@ -156,23 +165,22 @@ structureless medium for the given observation point ξ.
 `Vector{T}`
 
 ## Aliases
-    ∇φmol(Ξ::Vector{Vector{T}})
+    ∇φmol(Ξ::AbstractVector{Vector{T}})
 
 Computes the molecular potential gradients for a list of observation points.
 """
-function ∇φmol(
-        ξ      ::Vector{T},
-        charges::Vector{Charge{T}}
-    ) where T
-    isempty(charges) ? zeros(T, 3) :
-        -sum(q.val * (ξ .- q.pos) / euclidean(ξ, q.pos)^3 for q in charges)
+@inline function ∇φmol(
+    ξ::Vector{T},
+    charges::AbstractVector{Charge{T}}
+) where T
+    -sum(q.val * (ξ .- q.pos) / euclidean(ξ, q.pos)^3 for q in charges; init = zeros(T, 3))
 end
 
 @inline function ∇φmol(
-        Ξ      ::Vector{Vector{T}},
-        charges::Vector{Charge{T}}
-    ) where T
-    [∇φmol(ξ, charges) for ξ in Ξ]
+    Ξ::Union{AbstractVector{Vector{T}}, <: Base.Generator},
+    charges::AbstractVector{Charge{T}}
+) where T
+    ∇φmol.(Ξ, Ref(charges))
 end
 
 # Function stubs for potentials in submodules
