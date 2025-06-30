tkemmer/NESSie.jl

commit 9f632a7f6ce5e9c1616a31bb3fdf190401382433

BEM: fix type piracy from ImplicitArrays.jl
Fixes: #43

src/bem/implicit.jl
@@ -172,7 +172,7 @@ Everything from [`IterativeSolvers.gmres`]
 end
 
 function Base.:*(
-    A  ::InteractionMatrix{T},
+    A  ::InteractionMatrix{T, Vector{T}, Triangle{T}},
     x  ::AbstractVector{T}
 ) where T
     dst = zeros(T, size(A, 1))
@@ -187,7 +187,7 @@ function Base.:*(
 end
 
 function Base.:*(
-    A  ::InteractionMatrix{T},
+    A  ::InteractionMatrix{T, Vector{T}, Triangle{T}},
     B  ::SubArray{T, 2}
 ) where T
     m, n, p = (size(A)..., size(B, 2))

commit 0d34faa5c2a457a9f672ced1c405efdaf9186ca2

BEM: fix type piracy fix
The implicit Yukawa potential matrices have been broken since
9f632a7f6ce5e9c1616a31bb3fdf190401382433, where the patch to avoid
type piracy also prevented those matrices from using the intended
matrix-vector multiplication routines.

src/bem/implicit.jl
@@ -171,8 +171,13 @@ Everything from [`IterativeSolvers.gmres`]
     )
 end
 
+const _ImplicitBEMMatrix{T} = Union{
+    InteractionMatrix{T, Vector{T}, Triangle{T}},
+    InteractionMatrix{T, Vector{T}, TriangleQuad{T}}
+}
+
 function Base.:*(
-    A  ::InteractionMatrix{T, Vector{T}, Triangle{T}},
+    A  ::_ImplicitBEMMatrix{T},
     x  ::AbstractVector{T}
 ) where T
     dst = zeros(T, size(A, 1))
@@ -187,7 +192,7 @@ function Base.:*(
 end
 
 function Base.:*(
-    A  ::InteractionMatrix{T, Vector{T}, Triangle{T}},
+    A  ::_ImplicitBEMMatrix{T},
     B  ::SubArray{T, 2}
 ) where T
     m, n, p = (size(A)..., size(B, 2))
@@ -206,7 +211,7 @@ end
 
 @inline function LinearAlgebra.mul!(
     dst::AbstractArray{T, 1},
-    A  ::InteractionMatrix{T, Vector{T}, Triangle{T}},
+    A  ::_ImplicitBEMMatrix{T},
     B  ::AbstractArray{T}
 ) where T
     dst .= A * B