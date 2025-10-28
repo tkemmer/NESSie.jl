b5edb86964e3c1dffb81012af2d9e57a7fb50ace
Thomas Kemmer <thomas@binaryden.de>
2025-10-28 14:08:15 +0100

    Replace `TestItemRunner` by `ParallelTestRunner`

--- test/testmodel/born.jl
+++ test/testmodel/born.jl
@@ -1,36 +1,32 @@
-@testitem "Test model: Born" begin
-    include("../testsetup.jl")
+using NESSie.TestModel
 
-    using NESSie.TestModel
+const ion_names = [
+    "Li", "li", "Na", "na", "K", "k", "Rb", "rb", "Cs", "cs",
+    "Mg", "mg", "Ca", "ca", "Sr", "sr", "Ba", "ba"
+]
 
-    const ion_names = [
-        "Li", "li", "Na", "na", "K", "k", "Rb", "rb", "Cs", "cs",
-        "Mg", "mg", "Ca", "ca", "Sr", "sr", "Ba", "ba"
-    ]
-
-    @testset "defaultopt" begin
-        for T in testtypes
-            @test typeof(defaultopt(BornIon{T})) == Option{T}
-        end
+@testset "defaultopt" begin
+    for T in testtypes
+        @test typeof(defaultopt(BornIon{T})) == Option{T}
     end
-
-    @testset "bornion" begin
-        for T in testtypes, name in ion_names
-            @test typeof(bornion(name, T)) == BornIon{T}
-        end
-    end
-
-    @testset "Model" begin
-        for T in testtypes
-            let model = Model(bornion(first(ion_names), T))
-                @test model isa Model{T, Triangle{T}}
-                @test !isempty(model.nodes)
-                @test !isempty(model.elements)
-                @test !isempty(model.charges)
-                @test model.params == defaultopt(BornIon{T})
-            end
-        end
-    end
-
-    @test_skip espotential
 end
+
+@testset "bornion" begin
+    for T in testtypes, name in ion_names
+        @test typeof(bornion(name, T)) == BornIon{T}
+    end
+end
+
+@testset "Model" begin
+    for T in testtypes
+        let model = Model(bornion(first(ion_names), T))
+            @test model isa Model{T, Triangle{T}}
+            @test !isempty(model.nodes)
+            @test !isempty(model.elements)
+            @test !isempty(model.charges)
+            @test model.params == defaultopt(BornIon{T})
+        end
+    end
+end
+
+@test_skip espotential
